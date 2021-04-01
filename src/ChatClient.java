0fadf3ebb690d9fe908e47710d11bab5cc032dc5
Liam Watson <liam@watson.org.za>
2021-04-01 20:51:20 +0200

    Removed legacy comments

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -155,46 +155,6 @@ public class ChatClient extends JFrame implements ActionListener {
 
         login.setVisible(true);
 
-
-
-
-
-        //chatApp.setVisible(true);
-//        if (args.length != 1) {
-//            System.out.println("Usage: java QuoteClient <hostname>");
-//            return;
-//        }
-//
-//        Scanner in = new Scanner(System.in);
-//        String inpt = "";
-//
-//        System.out.println(args[0]);
-//        while(inpt != "q") {
-//            inpt = in.next();
-//            // get a datagram socket
-//            DatagramSocket socket = new DatagramSocket();
-//            // send request
-//            byte[] buf = new byte[256];
-//            buf = inpt.getBytes();
-//            InetAddress address = InetAddress.getByName(args[0]);
-//            DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
-//            socket.send(packet);
-//
-//            // get response
-//            buf = new byte[256];
-//            packet = new DatagramPacket(buf, buf.length);
-//            socket.receive(packet);
-//            // display response
-//            String received = new String(packet.getData(), 0, packet.getLength());
-//            System.out.println("From server: " + received);
-//            socket.close();
-//        }
-    //Scanner in = new Scanner(System.in);
-    //String inpt = "";
-    //Note here "register" is a place holder that will be determined by user button click @Luc
-    //joinServer(in.next(), in.next(), "register", InetAddress.getByName(args[0]));
-
-
     }
 
     public ChatClient() {
